2c072efa0a1c21d1bcf093377bb9dda07444ce07
Abel Quiros <beluchin@gmail.com>
2021-11-13 13:16:50 +0000

    the client management api impl for single-client approach

--- src/amps_rm/client.clj
+++ src/amps_rm/client.clj
@@ -7,10 +7,26 @@
 ;; e.g. client pool; new client per invocation; ...
 
 
-(defn disconnected [cmgr uri])
-(defn failed-to-connect [cmgr uri])
+(defn disconnected [cmgr uri]
+  (dissoc cmgr uri))
+
+(defn failed-to-connect [cmgr uri]
+  (dissoc cmgr uri))
 
 (defn ensure [cmgr uri promise]
-  (assoc cmgr uri promise))
+  (merge {uri promise} cmgr))
 
-(defn promise [cmgr uri])
+(defn promise [cmgr uri]
+  (cmgr uri))
+
+(comment
+  (import com.crankuptheamps.client.Client
+          com.crankuptheamps.client.exception.AMPSException)
+  (def uri "tcp://localhost:9007/amps/json")
+  (def p (delay (doto (Client. "unique client name")
+                  (.connect uri)
+                  (.logon))))
+  (def cmgr nil)
+  (def cmgr' (ensure cmgr uri p))
+  (def client @(promise cmgr' uri))
+  (.close client))
--- test/amps_rm/client_test.clj
+++ test/amps_rm/client_test.clj
@@ -4,4 +4,7 @@
 
 (t/deftest ensure-test
   (t/testing "no client - promise is cached"
-    (t/is (= {:uri :promise} (sut/ensure {} :uri :promise)))))
+    (t/is (= {:uri :promise} (sut/ensure {} :uri :promise))))
+
+  (t/testing "promise exists - do nothing"
+    (t/is (= {:uri :existing} (sut/ensure {:uri :existing} :uri :new)))))
