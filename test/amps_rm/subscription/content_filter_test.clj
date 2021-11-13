e10bee9071d9a873c443f86088a7ddf783e1e658
Abel Quiros <beluchin@gmail.com>
2021-11-13 22:16:49 +0000

    'AND' content filter to string - red

--- test/amps_rm/subscription/content_filter_test.clj
+++ test/amps_rm/subscription/content_filter_test.clj
@@ -2,3 +2,6 @@
   (:require [amps-rm.subscription.content-filter :as sut]
             [clojure.test :as t]))
 
+(t/deftest and-test
+  (t/testing "to string"
+    (t/is (= "(/a=1) AND (/b=2)" (sut/and "/a=1" "/b=2")))))
