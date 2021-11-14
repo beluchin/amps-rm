0bf672ececa6852f10e8e74d4122c038261b3f8c
Abel Quiros <beluchin@gmail.com>
2021-11-14 22:59:30 +0000

    or recombines and's - red

--- test/amps_rm/subscription/content_filter_test.clj
+++ test/amps_rm/subscription/content_filter_test.clj
@@ -8,4 +8,9 @@
 
 (t/deftest or-test
   (t/testing "to string"
-    (t/is (= "(/a=1) OR (/b=2)" (sut/to-string (sut/or "/a=1" "/b=2"))))))
+    (t/is (= "(/a=1) OR (/b=2)" (sut/to-string (sut/or "/a=1" "/b=2")))))
+
+  (t/testing "recombines and's"
+    (t/is (= "(/a=1) AND ((/b=2) OR (/c=3))"
+             (sut/to-string (sut/or (sut/and "/a=1" "/b=2")
+                                    (sut/and "/a=1" "/c=3")))))))
