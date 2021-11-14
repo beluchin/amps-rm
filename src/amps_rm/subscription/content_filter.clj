9f9ea1f5fd4364f69d374b2ab3571115eb75d66e
Abel Quiros <beluchin@gmail.com>
2021-11-14 22:46:40 +0000

    or to-string - green

--- src/amps_rm/subscription/content_filter.clj
+++ src/amps_rm/subscription/content_filter.clj
@@ -1,10 +1,22 @@
 (ns amps-rm.subscription.content-filter
-  (:refer-clojure :exclude [and or])
-  (:require [amps-rm.subscription.content-filter.impl :as impl]))
+  (:refer-clojure :exclude [and or]))
+
+(defprotocol ToString
+  (to-string [this] "the string that can be used on a subscription as the content filter"))
+(extend-protocol ToString
+  String
+  (to-string [this] this))
+
+(defrecord ^:private And [lhs rhs]
+  ToString
+  (to-string [_] (format "(%s) AND (%s)" (to-string lhs) (to-string rhs))))
+
+(defrecord ^:private Or [lhs rhs]
+  ToString
+  (to-string [_] (format "(%s) OR (%s)" (to-string lhs) (to-string rhs))))
 
 (defn and [cf1 cf2]
-  (impl/->And cf1 cf2))
+  (->And cf1 cf2))
 
-(defn or [cf1 cf2])
-
-(def to-string impl/to-string)
+(defn or [cf1 cf2]
+  (->Or cf1 cf2))
