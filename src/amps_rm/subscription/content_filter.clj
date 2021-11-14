c4bd5e49414fe4bbab6c9fc27a14f6d7be1746fa
Abel Quiros <beluchin@gmail.com>
2021-11-14 23:48:01 +0000

    intermediate

--- src/amps_rm/subscription/content_filter.clj
+++ src/amps_rm/subscription/content_filter.clj
@@ -1,5 +1,6 @@
 (ns amps-rm.subscription.content-filter
-  (:refer-clojure :exclude [and or]))
+  (:refer-clojure :exclude [and or])
+  (:require [clojure.set :as s]))
 
 (defprotocol ToString
   (to-string [this] "the string that can be used on a subscription as the content filter"))
@@ -15,8 +16,17 @@
   ToString
   (to-string [_] (format "(%s) OR (%s)" (to-string lhs) (to-string rhs))))
 
+(defn- try-to-recombine-into-and [and1 and2]
+  (let [set1 #{(:lhs and1) (:rhs and1)}
+        set2 #{(:lhs and2) (:rhs and2)}
+        intersection (s/intersection set1 set2)]
+    (if (not (empty? intersection))
+      (->And (single intersection) (->Or )))))
+
 (defn and [cf1 cf2]
-  (->And cf1 cf2))
+  (->And cf1 cf1))
 
 (defn or [cf1 cf2]
-  (->Or cf1 cf2))
+  (if (and (instance? And cf1) (instance? And cf2))
+    (try-to-recombine-into-and cf1 cf2)
+    (->Or cf1 cf2)))
