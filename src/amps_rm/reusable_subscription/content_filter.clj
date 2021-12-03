0dd89de0307ad4dcbab20ac29a5d5973895d9188
Abel Quiros <beluchin@gmail.com>
2021-12-03 11:29:25 +0000

    working on the recombining of the or into and - for multiple arguments

--- src/amps_rm/reusable_subscription/content_filter.clj
+++ src/amps_rm/reusable_subscription/content_filter.clj
@@ -1,5 +1,6 @@
 (ns amps-rm.reusable-subscription.content-filter
-  (:refer-clojure :exclude [and remove]))
+  (:refer-clojure :exclude [and or remove])
+  (:require [clojure.set :as set]))
 
 (defprotocol StringForm
   (string-form [cf] "the string that can be used on a subscription as the content filter"))
@@ -7,15 +8,31 @@
   String
   (string-form [cf] cf))
 
-(defrecord ^:private And [operands]
+(defrecord ^:private And [operand-set]
   StringForm
-  (string-form [_] (->> operands
+  (string-form [_] (->> operand-set
                         (map #(format "(%s)" (string-form %)))
                         (String/join " AND "))))
 
-(defrecord ^:private Or [operands])
+(defrecord ^:private Or [operand-set]
+  StringForm
+  (string-form [_] (->> operand-set
+                        (map #(format "(%s)" (string-form %)))
+                        (String/join " OR "))))
 
-(defn add [cf1 cf2])
+(defn- try-to-recombine-or-into-and [and-set]
+  (->Or and-set)
+  #_(let [sets (map :operand-set and-set)
+          intersection (set/intersection set1 set2)]
+      (if (seq intersection)
+        (->And (h/single intersection)
+               (->Or (h/single (set/difference set1 intersection))
+                     (h/single (set/difference set2 intersection))))
+        (->Or and1 and2))))
+
+(declare or)
+(defn add [cf1 cf2]
+  (or cf1 cf2))
 
 (defn and [& [_ _ & _ :as args]]
   (let [non-nil-args (set (filter some? args))]
@@ -23,5 +40,14 @@
       (if (= 1 (count non-nil-args))
         (first non-nil-args)
         (->And non-nil-args)))))
+
+(defn or [& [_ _ & _ :as args]]
+  (let [non-nil-args (set (filter some? args))]
+    (when (seq non-nil-args)
+      (if (= 1 (count non-nil-args))
+        (first non-nil-args)
+        (if (every? #(instance? And %) non-nil-args)
+          (try-to-recombine-or-into-and non-nil-args)
+          (->Or non-nil-args))))))
 
 (defn remove [cf to-remove])
--- test/amps_rm/reusable_subscription/content_filter_test.clj
+++ test/amps_rm/reusable_subscription/content_filter_test.clj
@@ -2,6 +2,10 @@
   (:require [amps-rm.reusable-subscription.content-filter :as sut]
             [clojure.test :as t]))
 
+(extend-protocol sut/StringForm
+  clojure.lang.Keyword
+  (string-form [this] (name this)))
+
 (t/deftest and-test
   (t/testing "string form"
     (t/is (#{"(/a=1) AND (/b=2)"
@@ -30,7 +34,10 @@
   (t/testing "recombines and's"
     (t/is (= (sut/and "/a=1" (sut/add "/b=2" "/c=3"))
              (sut/add (sut/and "/a=1" "/b=2")
-                      (sut/and "/a=1" "/c=3")))))
+                      (sut/and "/a=1" "/c=3"))))
+    (t/is (= (sut/and :a :b (sut/add :c :d))
+             (sut/add (sut/and :a :b :c)
+                      (sut/and :a :b :d)))))
 
   (t/testing "turns or's into in's"
     (throw (UnsupportedOperationException.)))
